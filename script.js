9978124e46bf6a6d54fcc471da5acbd76e21738b
Humzaza <humzaaamir61@gmail.com>
2022-06-04 18:14:27 -0400

    working on content section

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
+let button = document.querySelector('.add-Book');
+let content = document.querySelector('.bot-Content');
+let form = document.querySelector('.book-Form');
+let title = document.querySelector('#title').value;
+let author = document.querySelector('#author').value;
+let pages = document.querySelector('#pages').value;
+let read = document.querySelector('#read').value;
+
+
 let myLibrary = [];
+
+button.addEventListener('click', (e) => {
+    title = document.querySelector('#title').value;
+    author = document.querySelector('#author').value;
+    pages = document.querySelector('#pages').value;
+    read = document.querySelector('#read').value;
+    console.log(read);
+});
+button.addEventListener('click', addBookToLibrary(title, author, pages, read));
+
+
 
 function book (title, author, pages, read) {
     this.title = title;
@@ -10,7 +30,11 @@ function book (title, author, pages, read) {
     }
 }
 
-function addBookToLibrary() {
-    
+// let b1 = new book("huma1", "me", 100, true);
+// myLibrary.push(b1);
+// console.log(myLibrary[0]);
+
+function addBookToLibrary(title, author, pages, read) {
+    let newBook = new book(title, author, pages, read);
 }
 
