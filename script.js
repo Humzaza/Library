be2c49e77731d2ef84264a75d8d5c2ae1078f393
Humzaza <humzaaamir61@gmail.com>
2022-06-04 21:56:45 -0400

    user input complete working on displaying books

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let button = document.querySelector('.add-Book');
 let content = document.querySelector('.bot-Content');
 let form = document.querySelector('.book-Form');
-let title = document.querySelector('#title').value;
-let author = document.querySelector('#author').value;
-let pages = document.querySelector('#pages').value;
-let read = document.querySelector('#read').value;
+let title = document.querySelector('#title');
+let author = document.querySelector('#author');
+let pages = document.querySelector('#pages');
+let read = document.querySelector('input[name="read"]:checked').id;
 
 
 let myLibrary = [];
@@ -13,12 +13,9 @@ button.addEventListener('click', (e) => {
     title = document.querySelector('#title').value;
     author = document.querySelector('#author').value;
     pages = document.querySelector('#pages').value;
-    read = document.querySelector('#read').value;
-    console.log(read);
+    read = document.querySelector('input[name="read"]:checked').id;
+    addBookToLibrary(title, author, pages, read);
 });
-button.addEventListener('click', addBookToLibrary(title, author, pages, read));
-
-
 
 function book (title, author, pages, read) {
     this.title = title;
@@ -32,9 +29,29 @@ function book (title, author, pages, read) {
 
 // let b1 = new book("huma1", "me", 100, true);
 // myLibrary.push(b1);
-// console.log(myLibrary[0]);
+// console.log(contains(myLibrary, "huma17"));
+
+
+function contains (array, title) {
+    if (array.some(e => e.title === title)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+
+
+
 
 function addBookToLibrary(title, author, pages, read) {
     let newBook = new book(title, author, pages, read);
+    if(!contains(myLibrary, title)) {
+        myLibrary.push(newBook);
+    }
+    else {
+        alert("book already here");
+    }
 }
 
